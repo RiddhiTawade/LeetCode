b431bea606f318660b9c98843ddadfba10a29689
RiddhiTawade <tawaderiddhi05@gmail.com>
2026-09-14 18:34:47 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (51.49%) - LeetSync

---
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+
+        List<Integer> result = new ArrayList<>();
+
+        preorder(root, result);
+
+        return result;
+    }
+
+    public void preorder(TreeNode root, List<Integer> result) {
+
+        if (root == null) {
+            return;
+        }
+
+        // 1. Root
+        result.add(root.val);
+
+        // 2. Left
+        preorder(root.left, result);
+
+        // 3. Right
+        preorder(root.right, result);
+    }
+}
